2f8bab0802d6467010c12fd5d49e2c75da0dca5c
Anatoly Zhuravlev <newstreetpunk@yandex.ru>
2023-05-29 17:56:53 +0500

    parsel build

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+import defaultTheme from 'tailwindcss/defaultTheme'
+import colors from 'tailwindcss/colors'
+
+export const content = [
+  "./src/**/*.{html,js,ts,jsx,tsx}",
+]
+export const theme = {
+  container: {
+    center: true,
+    padding: '1.25rem',
+  },
+  extend: {},
+}
+export const plugins = []
+
