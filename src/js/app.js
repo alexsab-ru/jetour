b1854fd0e405daa867569e91aa82f6f8579fcb53
Alexander Sabanov <alexsab@mail.ru>
2023-05-30 09:51:53 +0400

    add info for jetour

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,16 @@
-import { add } from './module'
+import $ from 'jquery';
+// window.jQuery = require('jquery');
+// window.$ = require('jquery');
+// window.jQuery = $;
+// window.$ = $;
+import 'slick-carousel';
 
-add(4, 5)
+import './lp/jquery.fancybox.min.js';
+import './lp/jquery.inputmask.bundle.min.js';
+import './lp/index.js';
+import './lp/onpage.js';
+// import './lp/slick.min.js'
+
+// import { add } from './module'
+// add(4, 5)
+
