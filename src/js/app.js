94776b930e328380417f42d62fc46c8babce8923
Alexander Sabanov <alexsab@mail.ru>
2023-06-01 12:13:59 +0400

    upd files

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,9 @@
-// import $ from 'jquery';
 import 'slick-carousel';
-
-// window.jQuery = $;
-// window.$ = $;
 
 import './lp/jquery.fancybox.min.js';
 import './lp/jquery.inputmask.bundle.min.js';
 import './lp/index.js';
 import './lp/onpage.js';
-// import './lp/slick.min.js'
 
 // import { add } from './module'
 // add(4, 5)
